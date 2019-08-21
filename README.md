28931a1b9e81e66d5038e1eb5bb6b6013a6d943e
Martin Forstner <martin@redislabs.com>
2019-08-21 20:15:18 +0200

    add helper functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # redismq
-A Redis communication channel.
+This is an implementation of a messsage queue using Redis.
+
+It may be used as a library or as a gRPC client/server.
--- message_queue_client.py
+++ message_queue_client.py
@@ -1,4 +1,7 @@
+import json
+import logging
 import os
+import threading
 import uuid
 
 import grpc
@@ -10,6 +13,85 @@ from message_queue_pb2_grpc import MessageQueueStub
 MESSAGE_QUEUE_HOST = os.getenv('MESSAGE_QUEUE_HOST', 'localhost')
 MESSAGE_QUEUE_PORT = os.getenv('MESSAGE_QUEUE_PORT', '50051')
 MESSAGE_QUEUE_ADDRESS = '{}:{}'.format(MESSAGE_QUEUE_HOST, MESSAGE_QUEUE_PORT)
+
+
+def send_message(mq, service_name, func_name, params={}):
+    """
+    Put a message into a message queue.
+
+    :param mq: The message queue.
+    :param service_name: The name of the service.
+    :param func_name: The name fo the function.
+    :param params: The parameters.
+    :return: The payload of the response.
+    """
+    req_id = mq.send_req(service_name, func_name, json.dumps(params))
+    rsp = mq.recv_rsp(service_name, func_name, req_id, 1)
+    if rsp:
+        mq.ack_rsp(service_name, func_name, req_id, rsp)
+        return rsp
+    else:
+        raise TimeoutError()
+
+
+class Receivers(object):
+    """
+    Receivers class.
+    """
+
+    def __init__(self, mq, service_name, handler_funcs):
+        """
+        :param mq: a message queue
+        :param service_name: a service name
+        :param handler_funcs: a list of handler functions
+        """
+        self.mq = mq
+        self.service_name = service_name
+        self.handler_funcs = handler_funcs
+        self.threads = [threading.Thread(target=self._run,
+                                         name='{}.{}'.format(service_name, m.__name__),
+                                         args=(m,)) for m in handler_funcs]
+        self.running = False
+
+    def start(self):
+        """
+        Spawn a receiver thread for all handler functions.
+
+        :return: None
+        """
+        self.running = True
+        [t.start() for t in self.threads]
+
+    def stop(self):
+        """
+        Stop all receiver threads.
+
+        :return: None
+        """
+        self.running = False
+
+    def wait(self):
+        """
+        Wait for all receiver threads to finsih. N.B. This is a blocking operation.
+
+        :return: None
+        """
+        [t.join() for t in self.threads]
+
+    def _run(self, handler_func):
+
+        while self.running:
+
+            req_id, req_payload = self.mq.recv_req(self.service_name, handler_func.__name__, 1)
+            if req_payload:
+                try:
+                    rsp = handler_func(req_payload)
+                except Exception as e:
+                    logging.getLogger().error(str(e))
+                    continue
+
+                self.mq.ack_req(self.service_name, handler_func.__name__, req_id)
+                self.mq.send_rsp(self.service_name, handler_func.__name__, req_id, rsp)
 
 
 class MessageQueue(object):
