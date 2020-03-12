89e1c32f6ea7396af4be6f605f49baebff328d80
Martin Forstner <martin@redislabs.com>
2020-03-12 22:19:03 +0100

    add signal handler to test

--- tests/grpc/agent.py
+++ tests/grpc/agent.py
@@ -1,3 +1,4 @@
+import signal
 import threading
 
 from message_queue_client import Receivers, send_message
@@ -23,4 +24,14 @@ def test_func(req_params):
 
 threading.Thread(target=sender, args=('test-service', 'test_func')).start()
 
-rs = Receivers('test-service', [test_func]).start().wait()
+rs = Receivers('test-service', [test_func]).start()
+
+
+def signal_handler(signum, sighdl):
+    rs.stop()
+
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+rs.wait()
